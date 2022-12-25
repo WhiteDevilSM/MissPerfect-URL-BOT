b59f7b3c5744a5e6cb85aa66c8a31deaf959a79b
WHITE DEVIL - SM <105021702+WhiteDevilSM@users.noreply.github.com>
2022-12-25 22:17:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/WhiteDevilSM/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfect-URL-BOT
+cd /MissPerfectURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
