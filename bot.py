38c07d9bd82746b364189f0b669c47ce290ad7d5
WHITE DEVIL - SM <105021702+WhiteDevilSM@users.noreply.github.com>
2022-12-25 21:18:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 import os
 import logging
 from config import Config
-from pyrogram import Client as LazyDeveloper
+from pyrogram import Client as WhiteDevilSM
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = LazyDeveloper("@LazyDeveloper",
+    Warrior = WhiteDevilSM("@WhiteDevilSM",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
